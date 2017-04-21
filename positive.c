d232b15a9e18078c1be530ca96fb5144049ac60d
suryasadhana <suryasadhana74@gmail.com>
2017-04-21 15:35:10 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a;
+cin>>a;
+if(a>0)
+{
+cout<<"a is positive";
+}
+elseif(a<0)
+{
+cout<<"a is negative";
+}
+else
+{
+cout<<"a is zero";
+}
+}
